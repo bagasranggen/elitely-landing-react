570b1de80d6fcfcb53da25bb9feb69661d652372
Bagas Ranggen <bagas.ranggen@gmail.com>
2023-06-29 14:09:46 +0700

    Setup route

--- src/data/route.tsx
+++ src/data/route.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import AboutUs from "../pages/about-us";
+import Layout from "@/components/layout/Layout.tsx";
 
 export const route = createBrowserRouter([
     {
         path: '/about-us',
-        element: <AboutUs />,
+        element: <Layout>
+            <AboutUs />
+        </Layout>,
     }
-])
+]);
